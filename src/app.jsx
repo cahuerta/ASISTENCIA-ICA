788805700094287f723b981a9afd262a1b5aca63
cahuerta <cahuerta@gmail.com>
2025-07-28 19:38:33 -0400

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido al Instituto de Cirugía Articular</h1>
+      <p>Este es el frontend inicial usando React + Vite.</p>
+    </div>
+  )
+}
+
+export default App
