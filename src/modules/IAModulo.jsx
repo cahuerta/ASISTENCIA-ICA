ceabf673db9e427be02e2521fdaf51b85f3d693b
cahuerta <cahuerta@gmail.com>
2025-12-04 20:46:16 -0300

    Update IAModulo.jsx

--- src/modules/IAModulo.jsx
+++ src/modules/IAModulo.jsx
@@ -618,7 +618,8 @@ export default function IAModulo({ initialDatos, onIrPantallaTres }) {
 
       sessionStorage.setItem("iaJSON", JSON.stringify(iaJSON));
 
-     await fetch(`${BACKEND_BASE}/api/preview-informe`, {
+     await fetch(`${BACKEND_BASE}/api/guardar-datos-ia`, {
+
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
